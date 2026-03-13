fe441b87fdd5e697062242967cb44c1014491b16
den3110 <datistpham@gmail.com>
2026-03-13 21:43:55 +0700

    Revert to static HTML/CSS version

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // cần base này để GitHub Pages serve đúng dưới /introduce_alex/
-  base: '/introduce_alex/',
-})
